a5fe239ab77cf360d9101cb9eafc28f154766a80
pinmar <pinmar1980@outlook.com>
2018-02-21 22:04:38 +0100

    button menu

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -4,18 +4,13 @@ window.onscroll = function changeNav(){
   var nav = document.querySelector('.menu-button');
   var body = document.querySelector('body');
        console.log(body.getBoundingClientRect().top);
-        if((body.getBoundingClientRect().top) < 10){
+        if((body.getBoundingClientRect().top) < -10){
           nav.classList.add("bgchanged");
         } else {
-          console.log("change");
-          
+          if(nav.classList.contains("bgchanged")){
+            nav.classList.remove("bgchanged");
+          }
         }
-
-  // if(secondSectionTop <= navBarHeight) {
-  //       navBar.className = ('basic-classname');
-  // } else if(aboutSectionTop >= navBarHeight) {
-  //      navBar.className =  ('basic-classname added-classname');
-  // }
 }
 
 
