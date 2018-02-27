c2fba3197c5a33a1a50a6317b4d3e83295804218
pinmar <pinmar1980@outlook.com>
2018-02-27 11:19:34 +0100

    minor fixes

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,6 +5,15 @@ window.onscroll = function changeNav(){
   var body = document.querySelector('body');
         if((body.getBoundingClientRect().top) < -10){
           nav.classList.add("bgchanged");
+          let fired = false;
+          if( fired = false){
+            var imgDefer = document.querySelectorAll('.lazy');
+            for (var i=0; i<imgDefer.length; i++) {
+            if(imgDefer[i].getAttribute('data-src')) {
+            imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+            } }
+            fired = true;
+          }
         } else {
           if(nav.classList.contains("bgchanged")){
             nav.classList.remove("bgchanged");
